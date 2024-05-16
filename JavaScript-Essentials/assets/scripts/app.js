a2354749acdd375e2f6c7587e8782fc9eb7e9384
javohirhalimov <88170054+javohirhalimov@users.noreply.github.com>
2024-05-15 20:49:06 -0400

    JavaScript-Essentials
    Learned some core JavaScript

---
+++ JavaScript-Essentials/assets/scripts/app.js
@@ -0,0 +1,161 @@
+//import { apiKey, abc } from "./utils.js";
+
+// import * as util from "./utils.js";
+// console.log(util.abc);
+
+// let userMessage = "Hello World";
+
+// console.log(userMessage);
+
+// console.log(10 + 5);
+// console.log("Hello" + " World!");
+// console.log(10 === 5);
+
+// function createGreeting(userName, message = "Nice to meet you ") {
+//   //   console.log("Hello!");
+//   //   console.log(userName);
+//   //   console.log(message);
+//   return "Hi, I am " + userName + "." + message;
+// }
+
+// const greeting1 = createGreeting("Halimov");
+// console.log(greeting1);
+
+// greetUser("javohir@gmail.com", "Hello!");
+
+// export default (userName, message) => {
+//   console.log();
+// };
+
+// (userName) => {};
+
+// (number) => number * 3;
+
+// (number) => ({ age: number });
+
+// const userName = "Javohir";
+
+// const user = {
+//   name: "Javohir",
+//   age: 34,
+//   greet(userName) {
+//     console.log(this.age);
+//   },
+// };
+
+// console.log(user);
+// user.greet();
+
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
+//   greet() {
+//     console.log("Hello!" + this.name);
+//   }
+// }
+
+// const user1 = new User("Javohir", 25);
+// console.log(user1);
+// user1.greet();
+
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// console.log(hobbies);
+
+// hobbies.push("Working");
+// console.log(hobbies);
+
+// const index = hobbies.findIndex((item) => {
+//   return item === "Sports";
+// });
+
+// const indexNew = hobbies.findIndex((item) => item === "Sports");
+
+// console.log(index);
+// console.log(indexNew);
+
+// const editedHobbies = hobbies.map((item) => {
+//   return item + "!";
+// });
+// console.log(editedHobbies);
+
+// const objectEditedHobbies = hobbies.map((item) => ({ text: item }));
+// console.log(objectEditedHobbies);
+
+// const [firstName, lastName] = ["Javohir", "Halimov"];
+
+// // const firstName = userNameData[0];
+// // const lastName = userNameData[1];
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const { name: userName, age } = {
+//   name: "Javohir",
+//   age: 25,
+// };
+
+// console.log(userName);
+// console.log(age);
+
+// const hobbies = ["Sports", "Cooking"];
+// const user = {
+//   name: "Javohir",
+//   age: 24,
+// };
+
+// const newHobbies = ["Reading"];
+// const mergedHobbies = [...hobbies, ...newHobbies];
+// console.log(mergedHobbies);
+
+// const extendedUser = {
+//   isAdmin: true,
+//   ...user,
+// };
+// console.log(extendedUser);
+
+// // const password = prompt("Your password");
+// // if (password === "Hello") {
+// //   console.log("Hello Works");
+// // } else if (password === "hello") {
+// //   console.log("hello works");
+// // } else {
+// //   console.log("Access not granted!");
+// // }
+
+// const dishes = ["Palov", "Mantu"];
+
+// for (const dish of dishes) {
+//   console.log(dish);
+// }
+
+// const list = document.querySelector("ul");
+// list.remove();
+
+// function timeOut() {
+//   console.log("Time is out");
+// }
+
+// const timeOut2 = () => {
+//   console.log("Timed out ... again!");
+// };
+
+// setTimeout(timeOut, 2000);
+// setTimeout(() => {
+//   console.log("More timing out...");
+// }, 4000);
+
+// function greeter(greetFn) {
+//   greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
+
+// function init() {
+//   function greet() {
+//     console.log("Hello");
+//   }
+//   greet();
+// }
+// init();
---
+++ JavaScript-Essentials/assets/scripts/utils.js
@@ -0,0 +1,2 @@
+export let apiKey = "secretKey";
+export let abc = "alphabet";
