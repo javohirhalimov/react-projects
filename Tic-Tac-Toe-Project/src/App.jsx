018bf37a946841476085d01687766f39b6c6f423
javohirhalimov <88170054+javohirhalimov@users.noreply.github.com>
2024-06-03 20:05:21 -0400

    working on my project

--- Tic-Tac-Toe-Project/src/App.jsx
+++ Tic-Tac-Toe-Project/src/App.jsx
@@ -1,16 +1,54 @@
-import Player from "./components/Player.jsx"
-import GameBoard from "./components/GameBoard.jsx"
+import { useState } from "react";
+import Player from "./components/Player.jsx";
+import GameBoard from "./components/GameBoard.jsx";
+import Log from "./components/Log.jsx";
 
-function App() {
-  return <main>
-    <div id="game-container">
-      <ol id="players">
-        <Player initialName = "Player 1" symbol="X"/> 
-        <Player initialName = "Player 2" symbol="Y"/> 
-      </ol>
-      <GameBoard />
-    </div>
-  </main>
+function deriveActivePlayer(gameTurns) {
+
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+
 }
 
-export default App
+function App() {
+  const [gameTurns, setGameTurns] = useState([]);
+  //const [activePlayer, setActivePlayer] = useState("X");
+const activePlayer = deriveActivePlayer(gameTurns);
+  
+  function handleSelectSquare(rowIndex, colIndex) {
+   // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
+    
+   const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    };
+  
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="Y"
+            isActive={activePlayer === "O"}
+          />
+        </ol>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+      </div>
+      <Log turns={gameTurns} />
+    </main>
+  )};
+
+
+export default App;
--- Tic-Tac-Toe-Project/src/components/GameBoard.jsx
+++ Tic-Tac-Toe-Project/src/components/GameBoard.jsx
@@ -1,20 +1,28 @@
-import { useState } from "react";
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 
-export default function GameBoard() {
-const [gameBoard, setGameBoard] = useState(initialGameBoard)
+export default function GameBoard(onSelectSquare, turns) {
+    let gameBoard = initialGameBoard;
 
-function handleSelectSquare(rowIndex, colIndex, ) {
-    setGameBoard((prevGameBoard) => {
-        const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-        updateBoard[rowIndex][colIndex] = 'X';
-        return updateBoard;
-    });
-}
+    for (const turn of turns) {
+        const {square, player} = turn;
+        const {row, col} = square;
+
+        gameBoard[row][col] = player;
+    }
+// const [gameBoard, setGameBoard] = useState(initialGameBoard)
+
+// function handleSelectSquare(rowIndex, colIndex ) {
+//     setGameBoard((prevGameBoard) => {
+//         const updateBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+//         updateBoard[rowIndex][colIndex] = activePlayerSymbol;
+//         return updateBoard;
+//     });
+//     onSelectSquare();
+// }
 
   return (
     <ol id="game-board">
@@ -24,7 +32,9 @@ function handleSelectSquare(rowIndex, colIndex, ) {
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
                 
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>{playerSymbol}</button>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)} disabled = {playerSymbol !== null }>{playerSymbol}
+    
+                </button>
               </li>
             ))}
           </ol>
